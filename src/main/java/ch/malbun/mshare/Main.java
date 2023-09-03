8d5dd39e12fa77ee15077f85f8b8489af63c6f1f
Malbun <malbun08@mailfence.com>
2023-09-03 19:52:52 +0200

    Add Local IP Display

--- src/main/java/ch/malbun/mshare/Main.java
+++ src/main/java/ch/malbun/mshare/Main.java
@@ -7,27 +7,53 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
-import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
         final File[] file = {null};
+        String currentIP;
 
         Thread receiver = new receiver();
         receiver.start();
 
         Group root = new Group();
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(root, 400, 350);
+
+        //get local ip
+        ArrayList<String> ip = null;
+        try {
+            ip = new ArrayList<String>();
+            Enumeration<NetworkInterface> n = NetworkInterface.getNetworkInterfaces();
+            for (; n.hasMoreElements(); ) {
+                NetworkInterface e = n.nextElement();
+
+                Enumeration<InetAddress> a = e.getInetAddresses();
+                for (; a.hasMoreElements(); ) {
+                    InetAddress addr = a.nextElement();
+                    currentIP = addr.getHostAddress();
+                    if (!((currentIP.contains(":") || currentIP.contains("127.0.0.1")))) {
+                        ip.add(currentIP);
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
 
         //UI
         scene.setFill(Color.web("#444444"));
@@ -38,6 +64,25 @@ public class Main extends Application {
         choosenFile.setLayoutY(110);
         choosenFile.setFill(Color.LIGHTGRAY);
         root.getChildren().add(choosenFile);
+
+        //local ip
+        VBox ipHBox = new VBox();
+        for (String i : ip) {
+            Text ti = new Text(i);
+            ti.setFont(Font.font(16));
+            ti.setFill(Color.LIGHTGRAY);
+            ipHBox.getChildren().add(ti);
+        }
+        ipHBox.setLayoutX(200);
+        ipHBox.setLayoutY(220);
+        root.getChildren().add(ipHBox);
+
+        Text localIpText = new Text("Local IPs:");
+        localIpText.setFill(Color.LIGHTGRAY);
+        localIpText.setFont(Font.font(18));
+        localIpText.setLayoutX(200);
+        localIpText.setLayoutY(210);
+        root.getChildren().add(localIpText);
 
         //choose file
         FileChooser fileChooser = new FileChooser();
@@ -50,7 +95,8 @@ public class Main extends Application {
             try {
                 file[0] = fileChooser.showOpenDialog(stage);
                 choosenFile.setText(file[0].getName());
-            } catch (NullPointerException e) {}
+            } catch (NullPointerException e) {
+            }
         });
         root.getChildren().add(fileChooseButton);
 
